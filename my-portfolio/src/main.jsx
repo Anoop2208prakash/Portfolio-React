66f924e2bf479d86e99f86f7aa1257275287a098
Anoop Prakash <anoopprakash2208@gmail.com>
2026-02-17 20:00:50 +0530

    update

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
 import './index.css'
+import { router } from './router'
 
 // FIXED: Removed the "!" after getElementById('root')
 ReactDOM.createRoot(document.getElementById('root')).render(
